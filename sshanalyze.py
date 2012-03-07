41d34ef38f9b7ca5b3c7d28d52b3c39d67bfb79a
Will Hopper <whopper@cat.pdx.edu>
2012-03-07 00:37:35 -0800

    Rewrote main script. Added new classes 'sshcmds' and 'sshanalyze'

---
+++ sshanalyze.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# sshanalyze.py
+# analyze stats sent by remote host to screen for potential runaway processes
+# Written by Whopper
+# 3/7/12
+# _____________________________________________________________
+
+import sys, os, subprocess, commands, string
+
+
+class analyzer(object):
+
+  load_ave = 0
+  host = ''
+
+  def __init__(self, hostarg):
+    self.host = hostarg
+
+
+  def GetLoadAve(self):
+    StatFile = open('runtemp.txt')
+    GetData = []
+    GetData = StatFile.readlines()
+
+    FormatData = []
+    FormatData.append(GetData[2].rstrip('\r\n'))
+
+    LoadAverage = FormatData[0]
+    self.load_ave = float(LoadAverage[-4:])
+    if self.load_ave > 1.5:
+      self.Alert(self.host)
+
+  def Alert(self, hostarg):
+
+
+    print '_______________________________________________________________________'
+    print 'Warning: High Load Average'
+    print 'Machine: %s' % self.host
+    print "             CPU    %usr   %nice    %sys  %iowait   %irq   %soft   %steal  %guest  %idle"
+    subprocess.call(["cat runtemp.txt"], shell=True) 
+    subprocess.call(['echo "Users:"'], shell=True)
+    subprocess.call(['echo " "'], shell=True) 
+    subprocess.call(["ssh whopper@%s 'who | sort'" % self.host], shell=True)
+    print '_______________________________________________________________________' 
+
+
+if __name__ == '__main__':
+  main()
+
